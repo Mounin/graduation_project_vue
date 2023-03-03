b759ae84894c5b5c6ef37a1a561b892e4e11b95e
MOUNIN <2696660524@qq.com>
2023-03-03 17:37:48 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<!--  <header>-->
+<!--    <div class="wrapper">-->
+<!--      <nav>-->
+<!--        <RouterLink to="/">Index</RouterLink>-->
+<!--      </nav>-->
+<!--    </div>-->
+<!--  </header>-->
+  <RouterView />
+  <el-config-provider size="small" z-index="3000">
+  </el-config-provider>
+</template>
+
+<style scoped>
+</style>
